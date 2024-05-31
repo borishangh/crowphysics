10c3a0130022a3a66f7a5b2106b08b36069cafee
unknown <borishan.gh@gmail.com>
2024-05-31 10:56:56 +0530

    added articles

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,24 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/assets')
     eleventyConfig.addPassthroughCopy('./src/style.css')
 
+    eleventyConfig.addFilter('selectOnly', function (collection, tag) {
+        return collection.filter(item => {
+            if (item.data && item.data.tags) {
+                return item.data.tags.includes(tag);
+            }
+            return false;
+        });
+    });
+
+    eleventyConfig.addFilter('withTopic', function(collection, topicId) {
+        return collection.filter(item => {
+          if (item.data && item.data.topicId) {
+            return item.data.topicId === topicId;
+          }
+          return false;
+        });
+      });
+
     return {
         dir: {
             input: "src",
